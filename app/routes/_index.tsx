a614fe84a775ea7affef1888d1a9ef3e5a16afae
MartinGadea12 <martiingadeea1996@gmail.com>
2024-06-06 17:05:37 -0300

    landing page correccions

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import Footer from '~/components/footer'
+import React from 'react';
+import Footer from '~/components/footer';
 
 const features = [
   {
@@ -26,15 +26,18 @@ const features = [
       "Manage your personal or organization's holdings via CryptoGhost Portfolio.",
     icon: '🔥',
   },
-]
+];
 
 const Features = () => {
   return (
     <div className="w-full mt-20 md:mt-40 items-center rounded-lg p-5 md:w-1/2">
       <div className="grid grid-cols-1 gap-5">
         {features.map((feature, index) => (
-          <div key={index} className="flex items-start space-x-4">
-            <div className="text-[20px] text-[#04E6E6]">{feature.icon}</div>
+          <div
+            key={index}
+            className="flex items-start space-x-4 transform transition duration-500 hover:scale-105 hover:bg-opacity-50 hover:bg-teal-700 p-4 rounded-lg"
+          >
+            <div className="text-[20px] text-[#04E6E6] animate-pulse">{feature.icon}</div>
             <div>
               <h3 className="text-[20px] font-bold text-[#00E7D9]">
                 {feature.title}
@@ -47,13 +50,13 @@ const Features = () => {
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export const HeroSection = () => {
   return (
     <section className="flex w-full items-center justify-center px-4 py-16 text-white md:w-1/2 md:px-8">
-      <div className="mx-auto max-w-xl text-left">
+      <div className="mx-auto max-w-xl text-left animate-fade-in">
         <h1 className="font-inter mb-4 text-[36px] md:text-[48px] font-extrabold leading-tight text-[#F5F5F5]">
           10X Your insights, not your workload
         </h1>
@@ -64,13 +67,13 @@ export const HeroSection = () => {
         </p>
       </div>
     </section>
-  )
-}
+  );
+};
 
 const VerifySection = () => {
   return (
     <section className="mt-20 md:mt-40 text-white py-16 px-4 md:px-8 flex items-center justify-center">
-      <div className="max-w-4xl mx-auto text-center">
+      <div className="max-w-4xl mx-auto text-center animate-fade-in-up">
         <h1 className="font-inter font-extrabold text-[48px] leading-[58.09px] tracking-[0.06em] mb-4 text-[#F5F5F5]">
           Got a Hunch? <span className="text-teal-400">Verify it!</span>
         </h1>
@@ -88,7 +91,7 @@ const VerifySection = () => {
 export const SalesSection = () => {
   return (
     <section className="mt-20 md:mt-40 text-white py-16 px-4 md:px-8 flex items-center justify-center">
-      <div className="max-w-4xl mx-auto text-center">
+      <div className="max-w-4xl mx-auto text-center animate-fade-in-up">
         <h1 className="font-inter font-extrabold text-[36px] md:text-[48px] leading-[58.09px] tracking-[0.06em] mb-4 text-[#F5F5F5]">
           CryptoGhost query for crypto teams:
         </h1>
@@ -125,11 +128,11 @@ const intelligenceSections = [
 const IntelligenceSection = () => {
   return (
     <section className="text-white mt-20 md:mt-40">
-      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 animate-fade-in-up">
         {intelligenceSections.map((section, index) => (
-          <div               
+          <div
             key={index}
-            className="border border-[#04E6E6] rounded-3xl p-4"
+            className="border border-[#04E6E6] rounded-3xl p-4 transition duration-500 transform hover:scale-105 hover:bg-opacity-50 hover:bg-teal-700"
           >
             <h3 className="font-inter font-semibold text-[20px] leading-[24.2px] mb-4 text-[#F5F5F5]">
               {section.title}
@@ -147,7 +150,7 @@ const IntelligenceSection = () => {
 const AnalyticsSection = () => {
   return (
     <section className="mt-20 md:mt-40 text-white py-16 px-4 md:px-8 flex items-center justify-center">
-      <div className="max-w-4xl mx-auto text-center">
+      <div className="max-w-4xl mx-auto text-center animate-fade-in">
         <h1 className="font-inter font-extrabold text-[36px] md:text-[48px] leading-[58.09px] tracking-[0.06em] mb-4 text-[#F5F5F5]">
           See forward with CryptoGhost <br /> Blockchain Analytics
         </h1>
@@ -188,7 +191,7 @@ const logosTop = [
     src: 'https://cdn.prod.website-files.com/60118ca18674407b85935203/63fc65eff87ebe7b363a9676_Group-2.svg',
     alt: 'Polygon',
   },
-]
+];
 
 const logosBottom = [
   {
@@ -211,16 +214,16 @@ const logosBottom = [
     src: 'https://cdn.prod.website-files.com/60118ca18674407b85935203/6406e4ac9e2c94b8d843e5f3_Group%201597411.webp',
     alt: 'Gauntlet',
   },
-]
+];
 
 const BlockchainDataBanner = () => {
   return (
     <div className="mt-20 md:mt-40 p-10 text-white">
       <div className="flex flex-col md:flex-row max-w-7xl mx-auto text-left">
-        <h2 className="w-full text-center md:w-1/2 text-3xl font-extrabold text-white sm:text-4xl mb-4 md:mb-0">
+        <h2 className="w-full text-center md:w-1/2 text-3xl font-extrabold text-white sm:text-4xl mb-4 md:mb-0 animate-fade-in-left">
           The backbone for blockchain data
         </h2>
-        <div className="flex flex-col md:flex-row items-center justify-around w-full md:w-1/2">
+        <div className="flex flex-col md:flex-row items-center justify-around w-full md:w-1/2 animate-fade-in-right">
           <div className="text-center mb-4 md:mb-0">
             <span className="block text-4xl font-bold text-teal-400">
               300M+
@@ -247,15 +250,15 @@ const BlockchainDataBanner = () => {
         level.
       </p>
     </div>
-  )
-}
+  );
+};
 
 const OnchainDataPlatform = () => {
   return (
     <div className="flex min-h-screen flex-col justify-center bg-gradient-radial px-4">
       <div className="mt-10 flex w-full max-w-full flex-col items-center justify-center px-4">
         <div className="mb-16 w-full max-w-xl space-y-8 rounded-lg p-4 text-center">
-          <div className="inline-flex items-center rounded-full border border-teal-400 bg-transparent px-3 py-1 text-white transition duration-300 hover:border-teal-500 hover:bg-teal-500 hover:text-black">
+          <div className="inline-flex items-center rounded-full border border-teal-400 bg-transparent px-3 py-1 text-white transition duration-300 hover:border-teal-500 hover:bg-teal-500 hover:text-black animate-fade-in">
             <svg className="mr-2 h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4v16m8-8H4"></path>
             </svg>
@@ -266,20 +269,20 @@ const OnchainDataPlatform = () => {
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h14m-7-7l7 7-7 7"></path>
             </svg>
           </div>
-          <h1 className="text-4xl font-bold text-white md:text-5xl lg:text-6xl">
+          <h1 className="text-4xl font-bold text-white md:text-5xl lg:text-6xl animate-fade-in-up">
             Onchain Data Platform Trusted by the Best
           </h1>
-          <p className="mx-auto text-lg text-gray-300 md:text-xl">
+          <p className="mx-auto text-lg text-gray-300 md:text-xl animate-fade-in-up">
             Empowering crypto investors with deep insights to make informed decisions.
           </p>
           <div className="mt-8 flex flex-wrap justify-center space-x-4">
-            <button className="mb-2 flex items-center rounded-full bg-teal-400 px-4 py-2 text-black transition duration-300 hover:bg-teal-500 md:mb-0">
+            <button className="mb-2 flex items-center rounded-full bg-teal-400 px-4 py-2 text-black transition duration-300 hover:bg-teal-500 md:mb-0 animate-fade-in-up">
               Start for free
               <svg className="ml-2 h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h14m-7-7l7 7-7 7"></path>
               </svg>
             </button>
-            <button className="flex items-center rounded-full border border-teal-400 bg-transparent px-4 py-2 text-teal-400 transition duration-300 hover:border-teal-500 hover:bg-teal-500 hover:text-black">
+            <button className="flex items-center rounded-full border border-teal-400 bg-transparent px-4 py-2 text-teal-400 transition duration-300 hover:border-teal-500 hover:bg-teal-500 hover:text-black animate-fade-in-up">
               Explore Pricing
               <svg className="ml-2 h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h14m-7-7l7 7-7 7"></path>
@@ -288,7 +291,7 @@ const OnchainDataPlatform = () => {
           </div>
         </div>
         <div className="w-full text-center text-white mt-20">
-          <div className="mb-4 flex flex-wrap items-center justify-around bg-gradient-to-r from-[#022527] to-[#08858D]">
+          <div className="mb-4 flex flex-wrap items-center justify-around bg-gradient-to-r from-[#022527] to-[#08858D] animate-fade-in">
             {logosTop.map(logo => (
               <div className="rounded p-2" key={logo.alt}>
                 <img src={logo.src} alt={logo.alt} className="mx-2 my-4 h-6" />
@@ -297,12 +300,12 @@ const OnchainDataPlatform = () => {
           </div>
           <div className="my-8 flex items-center justify-center">
             <div className="h-[1px] flex-grow bg-gradient-to-r from-[#028080] to-[#04E6E6]"></div>
-            <p className="mx-4 text-lg md:text-xl">
+            <p className="mx-4 text-lg md:text-xl animate-fade-in-up">
               The smartest investors and crypto teams use CryptoGhost to win
             </p>
             <div className="h-[1px] flex-grow bg-gradient-to-r from-[#028080] to-[#04E6E6]"></div>
           </div>
-          <div className="flex flex-wrap items-center justify-around bg-gradient-to-r from-[#022527] to-[#08858D]">
+          <div className="flex flex-wrap items-center justify-around bg-gradient-to-r from-[#022527] to-[#08858D] animate-fade-in">
             {logosBottom.map(logo => (
               <div className="rounded p-2" key={logo.alt}>
                 <img src={logo.src} alt={logo.alt} className="mx-2 my-4 h-6" />
